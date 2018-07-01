0ecea41766eda23dd7ce4c35f20e8d6ff32aa720
sumit4204 <sumit.cool.im@gmail.com>
2018-07-01 15:40:05 +0530

    Update CreditRating.sol

--- CreditRating.sol
+++ CreditRating.sol
@@ -1,3 +1,4 @@
+pragma solidity ^0.4.18;
 
 contract Owned {
     address owner;
@@ -6,32 +7,33 @@ contract Owned {
         owner = msg.sender;
     }
     
-   modifier onlyOwner {
-       require(msg.sender == owner);
-       _;
-   }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 }
 
-contract CreditScore is Owned {
+contract CreditRating is Owned {
     
     struct Organization {
+        string orgId;
         string orgName;
-        uint orgId;
+    }
+    
+    mapping(address => Organization) organizations;
+    //address[] public orgAccts;
+    
+    function addOrganization(address orgAddress, string _orgName, string _orgId) onlyOwner {
+        var org = organizations[orgAddress];
+        org.orgName = _orgName;
+        org.orgId = _orgId;
         
+        //orgAccts.push(orgAddress);
     }
     
-    mapping(address => Organization) Organizations;
-
-    
-    function addOrganization(address org, string name) public {
-        if(msg.sender!= owner) return;
-        uint id = 1;
-        Organizations[org].orgName = name;
-        Organizations[org].orgId = id;
+    function getOrganization(address _orgAddress) view public returns (string, string) {
+        return (organizations[_orgAddress].orgName, organizations[_orgAddress].orgId);
     }
     
-    function getOrganization(address orgAddress) public returns (string) {
-        return Organizations[orgAddress].orgName;
-    }
-
+    
 }
